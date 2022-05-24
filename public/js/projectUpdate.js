cf348f2efab1022b3bf99e4099af11e76bd26e92
ChiChi <98738169+hergemony@users.noreply.github.com>
2022-05-24 12:03:34 +0800

    Update projectUpdate.js

--- public/js/projectUpdate.js
+++ public/js/projectUpdate.js
@@ -1,2 +1,4 @@
-// These script files handle front end requests from the user to make calls to our controllers to perform certain actions with the database and return that formattred information back to the view for the user to see.
+// These script files handle front end requests from the user to make calls to our controllers to perform certain actions with the database and return that formatted information back to the view for the user to see.
 // Such as button clicks to login, create a new medication, update a medication, delete a medication, add additions to that medication that we haven't added, etc.
+// Consider adding an area for user to upload a PHOTO of their medication cabinet/box or individual medications to serve as a collective reminder of what they already have.
+// Possible to allow user to create a SHOPPING LIST of items that need replenishment. Link to other e-commerce sites from the shopping list.
