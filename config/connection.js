9797fae903fb17d20c971c65652bc82af647a4ae
Origin 42 <daveplum1991@gmail.com>
2022-06-02 03:48:43 +0800

    connect to jawsdb

--- config/connection.js
+++ config/connection.js
@@ -3,19 +3,19 @@ require('dotenv').config();
 
 let sequelize;
 
-// create server for seqwuelize to sit on for connection to the database
 if (process.env.JAWSDB_URL) {
   sequelize = new Sequelize(process.env.JAWSDB_URL);
 } else {
-  process.env.DB_NAME,
-  process.env.DB_USER,
-  process.env.DB_PASSWORD,
-  {
-    host: 'localhost',
-    dialect: 'mysql',
-    // Port 3306 being used by sequelize
-    port: 3306,
-  }
+  sequelize = new Sequelize(
+    process.env.DB_NAME,
+    process.env.DB_USER,
+    process.env.DB_PASSWORD,
+    {
+      host: 'localhost',
+      dialect: 'mysql',
+      port: 3306,
+    }
+  );
 }
 
 module.exports = sequelize;
