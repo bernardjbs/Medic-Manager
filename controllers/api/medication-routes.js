70cdb5dce6a0a627f75b89c43868cbb38ede14c8
Bernard Sin Fat <bernardjbs@yahoo.com>
2022-06-02 16:03:22 +0800

    fixed delete medication which includes additions

--- controllers/api/medication-routes.js
+++ controllers/api/medication-routes.js
@@ -10,22 +10,22 @@ const { Medication, Addition } = require('../../models');
 router.get('/additions', async (req, res) => {
 
   const userMedsData = await Medication.findAll({
-      where: { 
-        user_id: req.session.user_id,
-      },
-      include: [{
-        model: Addition
-      }],
-      order: [['med_exp_date', 'DESC']],
+    where: {
+      user_id: req.session.user_id,
+    },
+    include: [{
+      model: Addition
+    }],
+    order: [['med_exp_date', 'DESC']],
   })
-  const userMeds = userMedsData.map((meds) => meds.get({plain: true}))
+  const userMeds = userMedsData.map((meds) => meds.get({ plain: true }))
 
   res.status(200).json({ userMeds, loggedIn: req.session.loggedIn })
 })
 
 // Creating route to create a new medication
 router.post('/', withAuth, async (req, res) => {
-  
+
   try {
     const newMedication = await Medication.create({
       ...req.body,
@@ -38,40 +38,39 @@ router.post('/', withAuth, async (req, res) => {
 });
 
 // Creating a route to delete an existing medication
-router.delete('/:id', withAuth, async ( req, res ) => {
+router.delete('/:id', withAuth, async (req, res) => {
 
   try {
-    console.log(req.params)
+    const additionData = await Addition.findAll({
+      where: {
+        medication_id: req.params.id,
+      },
+    })
+    console.log(additionData);
+    if (additionData) {
+      await Addition.destroy({ where: { medication_id: req.params.id } });
+    }
     const medicationData = await Medication.destroy({
       where: {
         id: req.params.id,
-      },
-      include: {
-        model: Addition,
-        where: {
-          medication_id: req.params.id
-        }
-      },
+      }
     });
 
-    if (!medicationData) {
-      res.status(404).json({ message: 'The medication you are tryig to delete could not be found, please try again' });
-    };
+    res.status(200).json(medicationData);
 
-    res.status(200).json(medicationData)
   } catch (err) {
     res.status(500).json({ message: 'Your request could not be performed, please try again', body: err });
   };
 });
 
 // Creating a route to update an existing medication 
-router.put('/:id', withAuth, async ( req, res ) => {
+router.put('/:id', withAuth, async (req, res) => {
 
   try {
     await Medication.update(req.body, { where: { id: req.params.id } });
     res.status(200).json({ message: 'Your medication details have successfully been updated' });
   } catch (err) {
-    res.status(400).json( { message: 'Your request could not be performed, please try again', body: err });
+    res.status(400).json({ message: 'Your request could not be performed, please try again', body: err });
   };
 });
 
